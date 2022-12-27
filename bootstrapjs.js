3cd0b6d5c4dd4a3f85897667129abf5444939a76
justin747 <justin747@gmail.com>
2022-12-26 23:32:24 -0800

    Linked JS file
    Linked JS file for console logging

---
+++ bootstrapjs.js
@@ -0,0 +1,7 @@
+let product = "shoe";
+let price   = 150;
+let qty     = 2;
+
+console.log(`You bought ${qty} pairs of ${product}s at $${price} each. Your grand total is $${qty * price}`);
+
+console.log(Math.floor(Math.random() * 20) + 1);
